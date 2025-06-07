3efa9274937ff5fac7bc71398c67b071253dbb93
gabriellagziel <gabriellagziel@gmail.com>
2025-06-07 17:50:54 +0200

    Add appointment details screen

---
+++ test/features/personal_scheduler/appointment_details_screen_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('AppointmentDetailsScreen shows appointment details', (tester) async {
+    // TODO: implement widget test
+  });
+}
